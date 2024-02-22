9e1f3b5bc91211db212e314c255ff172bf9eb02f
abhaysp95 <abhaysp95@protonmail.com>
2024-02-22 09:33:54 +0530

    feat: how to get zmq version

--- zmq_hello_world/server.cpp
+++ zmq_hello_world/server.cpp
@@ -1,9 +1,14 @@
+#include <zmq.h>
 #include <zmq.hpp>
 #include <iostream>
 
 using namespace std;
 
 int main(void) {
+	int major, minor, patch;
+	zmq_version(&major, &minor, &patch);
+	cout << "ZMQ version: " << major << "." << minor << "." << patch << '\n';
+
 	zmq::context_t ctx(2);
 	zmq::socket_t socket (ctx, zmq::socket_type::rep);
 	socket.bind("tcp://*:5555");
